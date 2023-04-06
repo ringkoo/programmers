bdb32d716134f01d4b919c89f465648954d50ba6
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-06 19:54:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,12 @@
+function solution(my_string, num1, num2) {
+    let arr = my_string.split('')
+    let tmp = arr[num1]
+    console.log('tmp = 1' , arr)
+    arr[num1] = arr[num2]
+    
+    arr[num2] = tmp
+    
+
+    return arr.join('')
+}
+console.log(solution("hello", 1, 2))
