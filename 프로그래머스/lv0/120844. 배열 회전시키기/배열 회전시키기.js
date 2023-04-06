c7e463a10e97b7ae3059f2f63da22dc4cbc0a83e
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-06 19:54:31 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,9 @@
+function solution(numbers, direction) {
+	if (direction === 'right') {
+		numbers.unshift(numbers.pop())
+	} else {
+		numbers.push(numbers.shift())
+	}
+	return numbers
+}
+console.log(solution([1, 2, 3], "right"))
