ca00080eeb4e3dac5236cfe131d2e6de8b9d7e1f
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-08 17:55:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,16 @@
+function solution(s) {
+	if (s.length === 4 || s.length === 6) {
+		if (parseInt(s) == s) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+
+
+
+console.log(solution("1000")) // flase
