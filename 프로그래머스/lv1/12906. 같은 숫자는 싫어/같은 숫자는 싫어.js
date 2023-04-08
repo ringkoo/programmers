e46153a543a9193c065e3d133603cc9672e7a77a
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-08 19:43:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 23.31 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,4 @@
-function solution(arr) {
-    let answer = []
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i + 1]) {
-            answer.push(arr[i])
-        }
-    }
-    return answer
+function solution(arr)
+{
+    return arr.filter((val,index) => val != arr[index+1]);
 }
-
-console.log(solution([1, 1, 3, 3, 0, 1, 1]))
