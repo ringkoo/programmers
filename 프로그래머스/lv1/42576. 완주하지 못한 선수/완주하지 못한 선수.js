2633bb43a148a1bd4791fd077237a5785b5314d3
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-10 15:04:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 150.93 ms, Memory: 57.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,12 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    console.log(participant)
+    console.log(completion)
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i];
+        }
+    }
+}
+console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])) // "mislav"
