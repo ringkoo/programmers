90c40cb898389d3a82f7ed26b2f249ca469701b6
DS <126160331+ringkoo@users.noreply.github.com>
2023-04-12 20:08:51 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.24 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    
+    
+    s = s.replace(/zero/gi, '0')
+    s = s.replace(/one/gi, '1')
+    s = s.replace(/two/gi, '2')
+    s = s.replace(/three/gi, '3')
+    s = s.replace(/four/gi, '4')
+    s = s.replace(/five/gi, '5')
+    s = s.replace(/six/gi, '6')
+    s = s.replace(/seven/gi, '7')
+    s = s.replace(/eight/gi, '8')
+    s = s.replace(/nine/gi, '9')
+    
+    //s의 맨앞에 나오는 문자나 숫자가 0일때 
+    
+    return parseInt(s)
+}
+
+console.log(solution("oneone23")) // 1478
