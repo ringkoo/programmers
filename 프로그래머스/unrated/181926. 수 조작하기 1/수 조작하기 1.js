b50638183362c9b663b4d6e209f1932e405a2f22
Daesik-Seo <126160331+ringkoo@users.noreply.github.com>
2023-07-28 16:18:38 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.43 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,23 @@
+function solution(n, control) {
+    for (let i = 0; i < control.length; i++) {
+        const command = control[i];
+
+        switch (command) {
+            case "w":
+                n += 1;
+                break;
+            case "s":
+                n -= 1;
+                break;
+            case "d":
+                n += 10;
+                break;
+            case "a":
+                n -= 10;
+                break;
+            default:
+                break;
+        }
+    }
+    return n;
+}
